d9f04463d6bff1ad1e12f2ba51d9382ac66f871c
victoriapong <victoria.pong@gmail.com>
2018-10-04 23:36:56 +0800

    Develop webpack with css-loader

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -11,5 +11,24 @@ module.exports = {
   },
   devServer: {
     contentBase: "dist"
+  },
+  module: {
+    rules: [
+      //add rules for loader
+      {
+        test: /\.css$/, //it take regular expression of filetype
+        use: [
+          // to specify your loaders
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          }
+          //it will run the css-loader first before pass to style-loader, which will
+          //inject css to html
+        ]
+      }
+    ]
   }
 };
--- src/main.js
+++ src/main.js
@@ -1,1 +1,1 @@
-alert("hello world");
+require("./main.css");
